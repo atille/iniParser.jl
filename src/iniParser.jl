289911cca6445f0f00d289e3fbe5dd39b4568390
Atille <gautierf@protonmail.com>
2018-05-14 16:32:39 +0200

    Update iniParser.jl
    Adding comments.

--- src/iniParser.jl
+++ src/iniParser.jl
@@ -7,8 +7,10 @@ module iniParser
 	export parseConf
 	export getparam
 
+	# return a IOStream flux as the f variable.
 	function parseConf(file::AbstractString)
 
+		# We check if the passed file exists.
 		if isfile(file) == false
 			println("File $file does not exist.")
 			quit()
@@ -19,18 +21,26 @@ module iniParser
 
 	end
 
+	# getparam function : return an AbstractString containing the 
+	# requested paramkey of the defined section on the conf container.
 	function getparam(container::IOStream, section::AbstractString, paramkey::AbstractString)
 
+		# defining globaly for later
 		current_section = ""
 		current_key = ""
 		current_value = ""
 
+		# for each line, we define if this is a section ; we keep it
+		# in memory, since we want things to converge to a defined section.
 		for ln in eachline(container)
 
 			if issection(ln) == true
 				current_section = getsection(ln)
 			end
-
+		
+			# we define if the line is a parameter. If true, then we make
+			# sure this is the section we want and the key we want.
+			# if yes, then we just return the value.
 			if isparam(ln) == true
 				current_key = getkey(ln)
 				current_value = getvalue(ln)
@@ -43,6 +53,8 @@ module iniParser
 		end
 	end
 
+	# Returns the key.
+	# Splits the = of the line and returns the first element.
 	function getkey(line::AbstractString)
 
 		if contains(line, " = ") == true
@@ -50,6 +62,8 @@ module iniParser
 		end
 	end
 
+	# Returns the value
+	# Splits the = of the line and returns the second element.
 	function getvalue(line::AbstractString)
 
 		if contains(line, " = ") == true
@@ -57,6 +71,8 @@ module iniParser
 		end
 	end
 
+	# To match the client request, we parse the section line containing
+	# [ and ] to make it more clear.
 	function getsection(line::AbstractString)
 		s = split(line, "[")[2]
 		s = split(s, "]")[1]
@@ -64,6 +80,7 @@ module iniParser
 		return s
 	end
 
+	# Define the line as a parameter one if it contains the = symbol.
 	function isparam(line::AbstractString)
 		if contains(line, " = ") == true
 			return true
@@ -74,6 +91,7 @@ module iniParser
 
 
 
+	# Define the line as a section one if it contains the [ and the ]
 	function issection(line::AbstractString)
 
 		if startswith(line, "[") && endswith(line, "]")
